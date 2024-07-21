fcc43f53d870c0989ade46c30d1be1ab556b43b5
primaam <pmaharyono@gmail.com>
2024-07-21 15:59:18 +0700

    update backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -34,10 +34,10 @@ ALLOWED_HOSTS = ["*"]
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTauthentication"
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES":[
-        "rest_framework.permissions.IsAuthenticated"
+        "rest_framework.permissions.IsAuthenticated",
     ]
 }
 
