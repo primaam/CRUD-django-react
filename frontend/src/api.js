61fe69f6b45da1a7183d97ae1a94a41a534440f6
primaam <pmaharyono@gmail.com>
2024-07-23 15:05:06 +0700

    update

---
+++ frontend/src/api.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "./constant";
+
+const baseURL = process.env.API_URL;
+
+const api = axios.create({
+    baseURL: baseURL,
+});
+
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default api;
