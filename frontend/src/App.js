5b92b9d40156ddedf504b7c4db96910a64d0e29a
primaam <pmaharyono@gmail.com>
2024-07-23 20:51:36 +0700

    update frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,35 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Register, Home, Login, NotFound } from "./pages";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
+    const Logout = () => {
+        localStorage.clear();
+        return <Navigate to="/login" />;
+    };
+
+    const RegisterAndLogout = () => {
+        localStorage.clear();
+        return <Register />;
+    };
+
     return (
-        <div>
-            <p>dhsgdhs</p>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <ProtectedRoute>
+                            <Home />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<RegisterAndLogout />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
